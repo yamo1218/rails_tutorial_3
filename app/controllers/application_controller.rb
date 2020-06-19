736d33aa85257d97e6a8e1e2beefa961098d18eb
yamo <tuuwb6b3fawtm9cxw7nb@docomo.ne.jp>
2020-06-20 00:43:00 +0900

    Finish static pages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,17 @@
-
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def hello
     render html: "hello, world!"
+  end
+
+  def help
+  end
+
+  def about
+  end
+
+  def contact
   end
 end
 
