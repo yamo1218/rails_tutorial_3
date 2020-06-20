7fa4856292a23112e1865606c58b4d8962c955cd
yamo <tuuwb6b3fawtm9cxw7nb@docomo.ne.jp>
2020-06-20 17:31:52 +0900

    tutorial7.3.1までok

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,18 @@
 class UsersController < ApplicationController
   def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(params[:user])
+    if @user.save
+      # 保存の成功をここで扱う。
+    else
+      render 'new'
+    end
+  end
+
+  def show
+    @user = User.find(params[:id])
   end
 end
